3b5c6d126737f1945b18c69ecc64121d14da7701
Saurabh Kumar Sharma <saurabhaasharma@gmail.com>
2025-12-14 21:26:27 +0530

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export defineConfig(() => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -20,4 +20,4 @@ export defineConfig(() => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
